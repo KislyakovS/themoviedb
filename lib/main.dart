ed3aa423ed62e08412497b5ce85425497d72a921
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-05-23 16:44:47 +0300

    Update brightness color

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        appBarTheme:
-            const AppBarTheme(color: Color(0xFF042541), centerTitle: true),
+        appBarTheme: const AppBarTheme(
+          color: Color(0xFF042541),
+          brightness: Brightness.dark,
+          centerTitle: true,
+        ),
         bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
           selectedItemColor: Color(0xFF042541),
           unselectedItemColor: Colors.grey,
         ),
+        scaffoldBackgroundColor: Colors.white,
         primarySwatch: Colors.blue,
       ),
       initialRoute: BottomNavigationScreen.routeName,
